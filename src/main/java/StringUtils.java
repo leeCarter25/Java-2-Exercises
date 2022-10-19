893cf2dc556e60f88edc25af3ced46beff2a3dc9
Christian Carter <carter.lee2028@gmail.com>
2022-10-19 16:34:27 -0500

    Dependency Management first push

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,6 @@
+public class StringUtils {
+    public static void main(String[] args) {
+        String test = org.apache.commons.lang3.StringUtils.upperCase("lowercasetouppercase");
+        System.out.println(test);
+    }
+}
